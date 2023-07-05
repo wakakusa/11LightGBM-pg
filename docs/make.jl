cf50fe4c723286685a02d7a9e858313eac79b7d3
wakakusa <wakakusaeng@outlook.jp>
2023-07-05 22:58:14 +0900

    init

---
+++ docs/make.jl
@@ -0,0 +1,20 @@
+using Documenter, LightGBM
+
+makedocs(;
+    modules=[LightGBM],
+    repo="https://github.com/IQVIA-ML/LightGBM.jl/blob/{commit}{path}#L{line}",
+    sitename="LightGBM.jl",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://IQVIA-ML.github.io/LightGBM.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+        "Functions" => "functions.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/IQVIA-ML/LightGBM.jl",
+)
